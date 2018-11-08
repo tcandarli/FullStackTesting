700473209f9d3a3f9de615cbba85bbf4d1d000f0
tcandarli <tcandarli@gmail.com>
2018-11-08 22:43:40 +0100

    spec5.js update

--- TestData/queries.js
+++ TestData/queries.js
@@ -1,6 +1,9 @@
-var queries = function() {
+var queries = function () {
 
     this.q1 = `select firstname, lastname, email from users where email = 'efewtrell8c@craigslist.org'`;
+    this.q2 = `select firstname, lastname, email, role, name 
+    from users as "u" inner join team as "t"
+    on u.team_id = t.id;`
 
 }
 
--- Tests/spec5.js
+++ Tests/spec5.js
@@ -1,7 +1,8 @@
 // Pages
 
 var home = require("../Pages/home.page.js");
-// var testData = require("../TestData/data.json")
+var topNav = require("../Pages/topNavigation.page.js");
+var self = require("../Pages/self.page.js");
 
 // DB Connection
 var pgp = require('pg-promise')( /*options*/ );
@@ -15,10 +16,13 @@ describe('Login with DB connection', () => {
     var username = '';
     var pass = '';
 
-    it('Test Case 4 - Connection String and Queries POM ', () => {
+    it('Test Case 5 - Backend Testing Single Page ', () => {
+
+        // Fetch the data from database
 
         db.any(queries.q1)
             .then((result) => {
+                arr = result;
                 username = result[0].email;
                 //console.log(username);
                 pass = result[0].firstname.toLowerCase() + result[0].lastname.toLowerCase();
@@ -33,8 +37,16 @@ describe('Login with DB connection', () => {
                 home.password.sendKeys(pass);
                 home.signinButton.click();
                 browser.sleep(2000);
-                expect(home.title.getText()).toEqual("VA");
+                // expect(home.title.getText()).toEqual("VA");
+                // browser.sleep(2000);
+                browser.actions().mouseMove(topNav.my).perform();
                 browser.sleep(2000);
+                topNav.self.click();
+                browser.sleep(2000);
+
+                expect(self.dataOnTable.get(0).getText()).toEqual(arr[0].firstname + " " + arr[0].lastname);
+                expect(self.dataOnTable.get(1).getText()).toEqual(arr[0].role);
+                // expect(self.dataOnTable.get(2).getText()).toEqual(arr[0].teamname);
             });
 
     });
